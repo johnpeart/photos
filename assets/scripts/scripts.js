4379a4f6ce719cfbda9833430464cef56ad1026d
johnpeart <johnpeart@users.noreply.github.com>
2021-01-17 11:50:11 +0000

    deploy: 36ec59e1f521008e1b4f5fcd0487c79e2fa9acf3

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -5,22 +5,3 @@ function toggleMenu() {
   var menu = document.getElementById("header-menu-toggle-image");
   menu.classList.toggle("open");
 }
-
-var accordian = document.getElementsByClassName("expand-button");
-var i;
-
-for (i = 0; i < accordian.length; i++) {
-  accordian[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
